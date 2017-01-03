b4c86de70fc21ea0a618bfc24cd6f88c69adf0ce
Benjamin Grenacher <bgrenacher@gmail.com>
2017-01-03 17:22:02 +0100

    http security headers

--- src/main/java/com/zuehlke/bgre/security/config/SecurityConfig.java
+++ src/main/java/com/zuehlke/bgre/security/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.zuehlke.bgre.security.domain.User;
 import com.zuehlke.bgre.security.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,8 +42,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .permitAll();
 
+        // Security Headers
+        // X-Content-Type-Options: nosniff set by default spring boot security
+        // Cache-Control: no-cache set by default spring boot security
         http
                 .headers()
-                .contentSecurityPolicy("default-src 'self'; script-src 'self' 'unsafe-inline'; report-uri /csp");
+                    .contentSecurityPolicy("default-src 'self'; script-src 'self' 'unsafe-inline'; report-uri /csp")
+                // HSTS (you may consider setting this header in the ssl handling part of your app e.g. apache, nginix)
+                .and()
+                    // be careful when deploying this 2 years policy because it will prevent your customers browsers from visiting your page without ssl
+                    .httpStrictTransportSecurity()
+                    .maxAgeInSeconds(63072000)
+                // HPKP (you may consider setting this header in the ssl handling part of your app e.g. apache, nginix)
+                .and()
+                    .httpPublicKeyPinning()
+                    .addSha256Pins("pGO1ErsUFSrId1hozlZOfyYOsE8mdiDgLyR89CtHK8E=")
+                    .maxAgeInSeconds(63072000)
+                    // remove reportOnly when certificates (including backup certificates!) including thoughtfully made deployment strategy worked out
+                    .reportOnly(true)
+                    .reportUri("/pkp");
     }
 }
