876e8350edfa3101583a7bb71ea359cbbb4956db
Benjamin Grenacher <bgrenacher@gmail.com>
2017-01-03 17:22:10 +0100

    renaming

--- src/main/java/com/zuehlke/bgre/security/config/GlobalCorsConfig.java
+++ src/main/java/com/zuehlke/bgre/security/config/GlobalCorsConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfig extends WebMvcConfigurerAdapter {
+public class GlobalCorsConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
